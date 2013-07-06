71fd3257e897cb5326504b3f986f26c9f80fac21
Nat Welch <nat@natwelch.com>
2013-07-06 12:58:30 -0700

    tweaking controller

--- app/controllers.rb
+++ app/controllers.rb
@@ -1,6 +1,14 @@
 Inspiration::App.controllers  do
   get :index do
     @images = []
+    rss_url = 'http://backend.deviantart.com/rss.xml?q=favby%3Acalvin166%2F1422412&type=deviation'
+    open(rss_url) do |rss|
+      feed = RSS::Parser.parse(rss)
+      feed.items.each do |item|
+        @images.push item.link
+      end
+    end
+
     render :index
   end
 end
