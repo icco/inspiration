c102e8f34b528426b9c11f26836e50070c49d9ed
Nat Welch <nat@natwelch.com>
2013-07-06 12:58:19 -0700

    change boot settings

--- config/boot.rb
+++ config/boot.rb
@@ -7,28 +7,15 @@ require 'rubygems' unless defined?(Gem)
 require 'bundler/setup'
 Bundler.require(:default, PADRINO_ENV)
 
-##
-# ## Enable devel logging
-#
-# Padrino::Logger::Config[:development][:log_level]  = :devel
-# Padrino::Logger::Config[:development][:log_static] = true
-#
-# ## Configure your I18n
-#
-# I18n.default_locale = :en
-#
-# ## Configure your HTML5 data helpers
-#
-# Padrino::Helpers::TagHelpers::DATA_ATTRIBUTES.push(:dialog)
-# text_field :foo, :dialog => true
-# Generates: <input type="text" data-dialog="true" name="foo" />
-#
-# ## Add helpers to mailer
-#
-# Mail::Message.class_eval do
-#   include Padrino::Helpers::NumberHelpers
-#   include Padrino::Helpers::TranslationHelpers
-# end
+require 'rss'
+require 'open-uri'
+
+## Enable devel logging
+Padrino::Logger::Config[:development][:log_level]  = :devel
+Padrino::Logger::Config[:development][:log_static] = true
+
+## Configure your I18n
+I18n.default_locale = :en
 
 ##
 # Add your before (RE)load hooks here
