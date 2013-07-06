aabb5f4ea5ced720a5dc761ae41ce082388d3eca
Nat Welch <nat@natwelch.com>
2013-07-06 12:08:01 -0700

    whee.

--- lib/scss_init.rb
+++ lib/scss_init.rb
@@ -5,8 +5,8 @@ module ScssInitializer
     # Store SCSS files (by default) within 'app/stylesheets'
     require 'sass/plugin/rack'
     Sass::Plugin.options[:syntax] = :scss
-    Sass::Plugin.options[:template_location] = Padrino.root("app/stylesheets")
-    Sass::Plugin.options[:css_location] = Padrino.root("public/stylesheets")
+    Sass::Plugin.options[:template_location] = Padrino.root("app/css")
+    Sass::Plugin.options[:css_location] = Padrino.root("public/css")
     app.use Sass::Plugin::Rack
   end
 end
