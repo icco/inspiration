1757d9e445f0cb48548695df4d79d7a7f9018fab
Nat Welch <nat@natwelch.com>
2013-07-06 19:33:48 -0700

    strip those links.

--- app/controllers.rb
+++ app/controllers.rb
@@ -1,6 +1,6 @@
 Inspiration::App.controllers  do
   get :index do
-    @images = Set.new(File.readlines(Inspiration::LINK_FILE))
+    @images = Set.new(File.readlines(Inspiration::LINK_FILE).map {|l| l.strip })
     rss_url = 'http://backend.deviantart.com/rss.xml?q=favby%3Acalvin166%2F1422412&type=deviation'
     open(rss_url) do |rss|
       feed = RSS::Parser.parse(rss)
