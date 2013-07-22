666206861c2fe18ae336afd5b3e71142e3163056
Nat Welch <nat@natwelch.com>
2013-07-21 20:57:38 -0700

    refactor

--- app/controllers.rb
+++ app/controllers.rb
@@ -12,20 +12,30 @@ Inspiration::App.controllers  do
   get :index, :cache => true do
     expires_in 3600 # 1 hr
 
-    @images = Set.new(File.readlines(Inspiration::LINK_FILE).map {|l| l.strip })
-    rss_url = 'http://backend.deviantart.com/rss.xml?q=favby%3Acalvin166%2F1422412&type=deviation'
-    open(rss_url) do |rss|
-      feed = RSS::Parser.parse(rss)
-      feed.items.each do |item|
-        @images.add item.link
-      end
-    end
-
-    all_images = @images.delete_if {|i| i.empty? }.to_a.sort
-    @images = all_images.sample(100)
-
-    File.open(Inspiration::LINK_FILE, 'w') {|file| file.write(all_images.to_a.join("\n")) }
+    @images = build_image_db.sample(100)
 
     render :index
   end
+
+  get :all do
+    @images = build_image_db
+
+    render :index
+  end
+end
+
+def build_image_db
+  images = Set.new(File.readlines(Inspiration::LINK_FILE).map {|l| l.strip })
+  rss_url = 'http://backend.deviantart.com/rss.xml?q=favby%3Acalvin166%2F1422412&type=deviation'
+  open(rss_url) do |rss|
+    feed = RSS::Parser.parse(rss)
+    feed.items.each do |item|
+      images.add item.link
+    end
+  end
+
+  all_images = images.delete_if {|i| i.empty? }.to_a.sort
+  File.open(Inspiration::LINK_FILE, 'w') {|file| file.write(all_images.to_a.join("\n")) }
+
+  return all_images
 end
