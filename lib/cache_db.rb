e15f56daff09733cb2fa88ed2e036842825106a1
Nat Welch <nat@natwelch.com>
2015-06-21 23:33:29 +0000

    basics

--- lib/cache_db.rb
+++ lib/cache_db.rb
@@ -4,7 +4,7 @@
 class CacheDB
 
   def initialize
-    @cache = JSON.parse(File.readlines(Inspiration::CACHE_FILE))
+    @cache = JSON.parse(File.read(Inspiration::CACHE_FILE))
   end
 
   def cache url
@@ -15,21 +15,46 @@ class CacheDB
     case url
     when dribbble_re
       oembed_url = "http://api.dribbble.com/shots/#{url.gsub(dribbble_re, "")}"
-      p oembed_url
+      # p oembed_url
 
-      # get oembed_url
-      # title = '"' + images.title + '" by ' + images.player.name
-      # if (images.image_400_url != undefined) {
-      #   image_link = images.image_400_url
-      # } else {
-      #   image_link = images.image_teaser_url
-      # }
+      resp = Faraday.get oembed_url
+      if resp.status == 200
+        data = JSON.parse(resp.body)
+      else
+        logger.error "Code #{resp.status}: Hitting #{oembed_url} for #{url}"
+        return
+      end
+
+      title = "\"#{data["title"]}\" by #{data["player"]["name"]}"
+      if data["image_400_url"]
+        image_link = data["image_400_url"]
+      else
+        image_link = data["image_teaser_url"]
+      end
+
+      hash = {title: title, image: image_link, size: {width: data["width"], height: data["height"]}}
+      p hash
     when deviant_re
-      oembed_url = "http://backend.deviantart.com/oembed?url=#{URI.escape(url, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}&format=jsonp"
-      p oembed_url
+      oembed_url = "http://backend.deviantart.com/oembed?url=#{URI.escape(url, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}&format=json"
+      # p oembed_url
+
+      resp = Faraday.get oembed_url
+      if resp.status == 200
+        data = JSON.parse(resp.body)
+      else
+        logger.error "Code #{resp.status}: Hitting #{oembed_url} for #{url}"
+        return
+      end
+
+      title = "\"#{data["title"]}\" by #{data["author_name"]}"
+      hash = {title: title, image: data["thumbnail_url"], size: {width: data["width"], height: data["height"]}}
+      p hash
     when flickr_re
       oembed_url = "http://www.flickr.com/services/oembed?url=#{URI.escape(url, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}&format=json&&maxwidth=300"
-      p oembed_url
+      # p oembed_url
+
+      resp = Faraday.get oembed_url
+      #p resp
 
       # title = '"' + images.title + '" by ' + images.author_name
       # image_url = images.thumbnail_url.replace(/\_s\./, "_n.")
