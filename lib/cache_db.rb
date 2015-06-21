2b8c6ac0523b5f8885b29bb6f2a7443495421e8e
Nat Welch <nat@natwelch.com>
2015-06-21 23:42:09 +0000

    fix hash building

--- lib/cache_db.rb
+++ lib/cache_db.rb
@@ -14,7 +14,7 @@ class CacheDB
 
     case url
     when dribbble_re
-      oembed_url = "http://api.dribbble.com/shots/#{url.gsub(dribbble_re, "")}"
+      oembed_url = "https://api.dribbble.com/shots/#{url.gsub(dribbble_re, "")}"
       # p oembed_url
 
       resp = Faraday.get oembed_url
@@ -33,9 +33,9 @@ class CacheDB
       end
 
       hash = {title: title, image: image_link, size: {width: data["width"], height: data["height"]}}
-      p hash
+      @cache[url] = hash
     when deviant_re
-      oembed_url = "http://backend.deviantart.com/oembed?url=#{URI.escape(url, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}&format=json"
+      oembed_url = "https://backend.deviantart.com/oembed?url=#{URI.escape(url, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}&format=json"
       # p oembed_url
 
       resp = Faraday.get oembed_url
@@ -48,16 +48,29 @@ class CacheDB
 
       title = "\"#{data["title"]}\" by #{data["author_name"]}"
       hash = {title: title, image: data["thumbnail_url"], size: {width: data["width"], height: data["height"]}}
-      p hash
+      @cache[url] = hash
     when flickr_re
-      oembed_url = "http://www.flickr.com/services/oembed?url=#{URI.escape(url, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}&format=json&&maxwidth=300"
+      oembed_url = "https://www.flickr.com/services/oembed?url=#{URI.escape(url, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}&format=json&&maxwidth=300"
       # p oembed_url
-
+      #
       resp = Faraday.get oembed_url
-      #p resp
+      if resp.status == 200
+        data = JSON.parse(resp.body)
+      else
+        logger.error "Code #{resp.status}: Hitting #{oembed_url} for #{url}"
+        return
+      end
 
-      # title = '"' + images.title + '" by ' + images.author_name
-      # image_url = images.thumbnail_url.replace(/\_s\./, "_n.")
+      if !data["thumbnail_url"]
+        logger.error "No Tumbnail: #{oembed_url}"
+        return
+      end
+
+      image_url = data["thumbnail_url"].gsub(/\_s\./, "_n.")
+      title = "\"#{data["title"]}\" by #{data["author_name"]}"
+      hash = {title: title, image: image_url, size: {width: data["width"], height: data["height"]}}
+      p hash
+      @cache[url] = hash
     else
       logger.error "No idea what url this is: #{url}"
     end
