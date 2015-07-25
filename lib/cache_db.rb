44b79ed502507a434334232ae2f5c66c107e6ee5
Nat Welch <nat@natwelch.com>
2015-07-25 19:51:47 +0000

    rescue from failed json

--- lib/cache_db.rb
+++ lib/cache_db.rb
@@ -28,66 +28,70 @@ class CacheDB
     deviant_re = %r{deviantart\.com}
     flickr_re = %r{www\.flickr\.com}
 
-    case url
-    when dribbble_re
-      # Dribbble does not like us, go slow
-      sleep 1 + rand
+    begin
+      case url
+      when dribbble_re
+        # Dribbble does not like us, go slow
+        sleep rand
 
-      oembed_url = "https://api.dribbble.com/shots/#{url.gsub(dribbble_re, "")}"
-      resp = Faraday.get oembed_url
-      if resp.status == 200
-        data = JSON.parse(resp.body)
+        oembed_url = "https://api.dribbble.com/shots/#{url.gsub(dribbble_re, "")}"
+        resp = Faraday.get oembed_url
+        if resp.status == 200
+          data = JSON.parse(resp.body)
+        else
+          logger.error "Code #{resp.status}: Hitting #{oembed_url} for #{url}"
+          return
+        end
+
+        title = "\"#{data["title"]}\" by #{data["player"]["name"]}"
+        if data["image_400_url"]
+          image_link = data["image_400_url"]
+        else
+          image_link = data["image_teaser_url"]
+        end
+
+        attrs = {title: title, image: image_link, size: {width: data["width"], height: data["height"]}}
+        hash.merge! attrs
+      when deviant_re
+        oembed_url = "https://backend.deviantart.com/oembed?url=#{URI.escape(url, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}&format=json"
+        resp = Faraday.get oembed_url
+        if resp.status == 200
+          data = JSON.parse(resp.body)
+        else
+          logger.error "Code #{resp.status}: Hitting #{oembed_url} for #{url}"
+          return
+        end
+
+        title = "\"#{data["title"]}\" by #{data["author_name"]}"
+        attrs = {title: title, image: data["thumbnail_url"], size: {width: data["width"], height: data["height"]}}
+        hash.merge! attrs
+      when flickr_re
+        oembed_url = "https://www.flickr.com/services/oembed?url=#{URI.escape(url, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}&format=json&&maxwidth=300"
+        resp = Faraday.get oembed_url
+        if resp.status == 200
+          data = JSON.parse(resp.body)
+        else
+          logger.error "Code #{resp.status}: Hitting #{oembed_url} for #{url}"
+          return
+        end
+
+        if !data["thumbnail_url"]
+          logger.error "No Tumbnail for #{url} at #{oembed_url}"
+          return
+        end
+
+        image_url = data["thumbnail_url"].gsub(/\_s\./, "_n.")
+        title = "\"#{data["title"]}\" by #{data["author_name"]}"
+        attrs = {title: title, image: image_url, size: {width: data["width"], height: data["height"]}}
+        hash.merge! attrs
       else
-        logger.error "Code #{resp.status}: Hitting #{oembed_url} for #{url}"
-        return
+        logger.error "No idea what url this is: #{url}"
       end
 
-      title = "\"#{data["title"]}\" by #{data["player"]["name"]}"
-      if data["image_400_url"]
-        image_link = data["image_400_url"]
-      else
-        image_link = data["image_teaser_url"]
-      end
-
-      attrs = {title: title, image: image_link, size: {width: data["width"], height: data["height"]}}
-      hash.merge! attrs
-    when deviant_re
-      oembed_url = "https://backend.deviantart.com/oembed?url=#{URI.escape(url, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}&format=json"
-      resp = Faraday.get oembed_url
-      if resp.status == 200
-        data = JSON.parse(resp.body)
-      else
-        logger.error "Code #{resp.status}: Hitting #{oembed_url} for #{url}"
-        return
-      end
-
-      title = "\"#{data["title"]}\" by #{data["author_name"]}"
-      attrs = {title: title, image: data["thumbnail_url"], size: {width: data["width"], height: data["height"]}}
-      hash.merge! attrs
-    when flickr_re
-      oembed_url = "https://www.flickr.com/services/oembed?url=#{URI.escape(url, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}&format=json&&maxwidth=300"
-      resp = Faraday.get oembed_url
-      if resp.status == 200
-        data = JSON.parse(resp.body)
-      else
-        logger.error "Code #{resp.status}: Hitting #{oembed_url} for #{url}"
-        return
-      end
-
-      if !data["thumbnail_url"]
-        logger.error "No Tumbnail for #{url} at #{oembed_url}"
-        return
-      end
-
-      image_url = data["thumbnail_url"].gsub(/\_s\./, "_n.")
-      title = "\"#{data["title"]}\" by #{data["author_name"]}"
-      attrs = {title: title, image: image_url, size: {width: data["width"], height: data["height"]}}
-      hash.merge! attrs
-    else
-      logger.error "No idea what url this is: #{url}"
+      return set url, hash
+    rescue Exception => e
+      logger.error "Failed #{oembed_url} for #{url}: #{e.inspect}"
     end
-
-    return set url, hash
   end
 
   def get url
