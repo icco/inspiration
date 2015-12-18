b20ec0a450d80b3d3535713e4cbccaa83882d1ca
Nat Welch <nat@natwelch.com>
2015-12-18 04:49:00 +0000

    rubocop -a

--- lib/image_db.rb
+++ lib/image_db.rb
@@ -221,7 +221,7 @@ class ImageDB
 
     # Twitter
     twitter_collect_with_max_id do |max_id|
-      options = {count: 200, include_rts: true}
+      options = { count: 200, include_rts: true }
       options[:max_id] = max_id unless max_id.nil?
       print_data = { twitter: "icco", max_id: max_id }
       logging.info print_data.inspect
@@ -249,7 +249,7 @@ class ImageDB
     true
   end
 
-  def twitter_collect_with_max_id(collection=[], max_id=nil, &block)
+  def twitter_collect_with_max_id(collection = [], max_id = nil, &block)
     response = yield(max_id)
     collection += response
     response.empty? ? collection.flatten : twitter_collect_with_max_id(collection, response.last.id - 1, &block)
