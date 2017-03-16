c2669cabab58d83c9197e7d3849fb431fcd22485
Nat Welch <nat@natwelch.com>
2017-03-15 20:13:28 -0700

    fix archive script

--- archive.sh
+++ archive.sh
@@ -2,5 +2,6 @@
 
 for u in $(cat links.txt); do
   echo $u
-  curl -s --data-urlencode "run=1&url=$u" https://archive.is/submit/ | grep -i archive.is | awk -F\" '{ print $2 }'
+  curl -sL --data-urlencode "url=$u" https://archive.is/submit/ > /dev/null
+  echo $?
 done
