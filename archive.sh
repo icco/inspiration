bad4ed7ae82f492c0b722015a21b93085a494f31
Nat Welch <nat@natwelch.com>
2019-03-17 14:56:30 +0000

    archive two places

--- archive.sh
+++ archive.sh
@@ -2,13 +2,10 @@
 
 sort --help | grep -i random > /dev/null
 
-if [[ $? -eq 1 ]]; then
-  LINKS=$(cat links.txt)
-else
-  LINKS=$(cat links.txt | sort -R | head -n 30)
-  echo " -- Randomized Links."
-fi
+LINKS=$(cat links.txt | sort -R | head -n 30)
+echo " -- Randomized Links."
 
 for u in $LINKS; do
+  curl -s -I -H "Accept: application/json" "https://web.archive.org/save/${u}" | grep '^x-cache-key:' | sed "s,https,&://,; s,\(${u}\).*$,\1,"
   curl -sv --data-urlencode "url=$u" https://archive.is/submit/ 2>&1 >/dev/null | grep '< HTTP/1.1' | grep -v 302
 done
