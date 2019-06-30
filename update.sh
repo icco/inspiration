80bd4c6e994182f35275dd9164288ae59a8c3138
Nat Welch <nat@natwelch.com>
2019-06-30 00:03:38 +0000

    update stuff

--- update.sh
+++ update.sh
@@ -12,12 +12,19 @@ source $RUBY
 git pull
 bundle update
 git ci Gemfile* -m 'bundle update'
+
 echo "" > links.txt
 rake update_links
-git ci links.txt -m 'update links'
+rm links.json
+for l in $(cat links.txt); do echo "{\"url\": \"$l\"}" >> links.json; done
+git ci links.* -m 'update links'
+
 rake build_cache
 git ci cache.* -m 'update cache'
 rake clean
 git ci cache.* -m 'clean cache'
 git st
 git push
+
+bq load --autodetect --source_format=NEWLINE_DELIMITED_JSON inspiration.data cache.json
+bq load --time_partitioning_field=DAY --autodetect --source_format=NEWLINE_DELIMITED_JSON inspiration.links links.json
