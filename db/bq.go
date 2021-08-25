b951fa873d98e09ba02eea3ddb01151ef01853a6
Nat Welch <nat@natwelch.com>
2021-08-25 04:33:04 +0000

    it builds

---
+++ db/bq.go
@@ -0,0 +1,87 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"cloud.google.com/go/bigquery"
+	"google.golang.org/api/iterator"
+)
+
+const (
+	project = "icco-cloud"
+)
+
+type Entry struct {
+	Size     Size
+	Image    string
+	Title    string
+	Modified time.Time
+	URL      string
+}
+
+type Size struct {
+	Height int64
+	Width  int64
+}
+
+type countResponse struct {
+	Cnt int64
+}
+
+func Count(ctx context.Context) (int64, error) {
+	client, err := bigquery.NewClient(ctx, project)
+	if err != nil {
+		return 0, err
+	}
+
+	query := client.Query("SELECT count(*) as cnt FROM `icco-cloud.inspiration.cache`")
+	it, err := query.Read(ctx)
+	if err != nil {
+		return 0, err
+	}
+
+	for {
+		var c countResponse
+		err := it.Next(&c)
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return 0, err
+		}
+
+		return c.Cnt, nil
+	}
+
+	return 0, fmt.Errorf("got to impossible state")
+}
+
+func Page(ctx context.Context, n int64) ([]*Entry, error) {
+	client, err := bigquery.NewClient(ctx, project)
+	if err != nil {
+		return nil, err
+	}
+
+	query := client.Query("SELECT * FROM `icco-cloud.inspiration.cache` WHERE url is not null ORDER BY rand() * EXTRACT(DAYOFYEAR FROM CURRENT_DATE()) LIMIT @per_page OFFSET @offset")
+	_, err = query.Read(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return nil, fmt.Errorf("not implemented")
+}
+
+func Get(ctx context.Context, urls []string) ([]*Entry, error) {
+	client, err := bigquery.NewClient(ctx, project)
+	if err != nil {
+		return nil, err
+	}
+
+	query := client.Query("SELECT * FROM `icco-cloud.inspiration.cache` WHERE url IN UNNEST(@urls)")
+	_, err = query.Read(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return nil, fmt.Errorf("not implemented")
+}
--- go.mod
+++ go.mod
@@ -3,10 +3,19 @@ module github.com/icco/inspiration
 go 1.16
 
 require (
+	cloud.google.com/go v0.93.3 // indirect
+	cloud.google.com/go/bigquery v1.21.0
+	cloud.google.com/go/datacatalog v0.1.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/go-chi/chi/v5 v5.0.3
+	github.com/googleapis/gax-go/v2 v2.1.0 // indirect
 	github.com/icco/gutil v0.0.0-20210706020035-d1bef041b1ac
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/zap v1.19.0
+	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
+	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f // indirect
 	golang.org/x/sys v0.0.0-20210823070655-63515b42dcdf // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/api v0.54.0
+	google.golang.org/genproto v0.0.0-20210824181836-a4879c3d0e89 // indirect
 )
